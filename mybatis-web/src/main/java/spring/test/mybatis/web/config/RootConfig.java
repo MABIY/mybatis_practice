f3f115ab586dda21dfed3e60a140f3f5bec6ff63
lh <jyfc8979@gmail.com>
2019-08-11 11:21:13 +0800

    ..

--- mybatis-web/src/main/java/spring/test/mybatis/web/config/RootConfig.java
+++ mybatis-web/src/main/java/spring/test/mybatis/web/config/RootConfig.java
@@ -2,6 +2,7 @@ package spring.test.mybatis.web.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import spring.test.mybatis.domain.service.impl.Components;
 
 /**
